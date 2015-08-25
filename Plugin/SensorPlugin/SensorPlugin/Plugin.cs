1ce2736bfcc3a3228272b0f00cab92f4c0a48947
Dinko <jakovljevic.dinko@gmail.com>
2015-08-25 13:13:45 +0200

    Register Sensor Plugin
    Register Sensor Plugin in ResQMap Plugin register

--- Plugin/SensorPlugin/SensorPlugin/Plugin.cs
+++ Plugin/SensorPlugin/SensorPlugin/Plugin.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using Carmenta.ResQMapAPI.Plugins.CustomEntities.CustomEntityDefinitions;
+using Carmenta.ResQMapAPI.Plugins.Registrars;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorPlugin
+{
+    public class Plugin : Carmenta.ResQMapAPI.Plugins.Plugin
+    {
+        protected override void RegisterCustomPanels(CustomPanelRegistrar registrar)
+        {
+            //registrar.Register(
+            //    "Sensors",
+            //    typeof(SensorPanel),
+            //    CustomPanelOrientation.Left
+            //    //Resource1.slika
+            //    );
+
+            base.RegisterCustomPanels(registrar);
+        }
+    }
+}
